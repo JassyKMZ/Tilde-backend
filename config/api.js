be7c1b97361906c0eb4396e497b6a86b6849609a
JassyKMZ <j.kucharz@kmz-sbk.de>
2025-10-24 11:28:58 +0200

    Add files via upload

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 module.exports = {
   rest: {
     defaultLimit: 100, // Change entries per page default limit
-    maxLimit: 100,
+    maxLimit: 100, // Change max limit of entries per page, default: 100
     withCount: true,
   },
 };
