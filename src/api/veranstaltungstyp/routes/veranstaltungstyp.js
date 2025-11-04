c7a4611d87a6288ac4dc4a5f8869e9719218c8a4
JassyKMZ <j.kucharz@kmz-sbk.de>
2025-11-04 12:34:38 +0100

    Delete src/api/veranstaltungstyp directory

--- src/api/veranstaltungstyp/routes/veranstaltungstyp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * veranstaltungstyp router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::veranstaltungstyp.veranstaltungstyp');
--- src/api/veranstaltungstyp/services/veranstaltungstyp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * veranstaltungstyp service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::veranstaltungstyp.veranstaltungstyp');
