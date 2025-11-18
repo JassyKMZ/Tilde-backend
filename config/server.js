459cb7e5a9b9f8ba1792856a9a1bfcd4b72d4793
JassyKMZ <j.kucharz@kmz-sbk.de>
2025-11-18 15:43:06 +0100

    Add files via upload

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("PUBLIC_URL"),
+  url: env("FRONTEND_URL", "https://tilde-app.de"),
   app: {
     keys: env.array("APP_KEYS"),
   },
