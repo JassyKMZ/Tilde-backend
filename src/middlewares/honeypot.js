083b5b17c9b60bdf453c02fd83343f6ea451c69c
JassyKMZ <j.kucharz@kmz-sbk.de>
2025-12-01 15:15:38 +0100

    Delete src/middlewares directory

--- src/middlewares/honeypot.js
+++
@@ -1,8 +0,0 @@
-module.exports = (config, { strapi }) => {
-  return async (ctx, next) => {
-    if (ctx.request.body?.data?.website) {
-      ctx.throw(400, "Bot detected (honeypot field filled).");
-    }
-    await next();
-  };
-};
