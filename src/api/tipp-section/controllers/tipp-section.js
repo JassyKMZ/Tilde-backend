8f93686cf02a9470c1476f837de79b6d81e1e89f
JassyKMZ <203271569+JassyKMZ@users.noreply.github.com>
2026-09-21 12:03:14 +0200

    Delete src/api/tipp-section directory

--- src/api/tipp-section/controllers/tipp-section.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tipp-section controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::tipp-section.tipp-section');
