39ad40089b9872595d32fb474d81bf9b0a362893
JassyKMZ <203271569+JassyKMZ@users.noreply.github.com>
2026-09-22 14:24:09 +0200

    deleted tipp-section

---
+++ api/user-profile/routes/preferences.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  routes: [
+    // {
+    //   method: "PUT",
+    //   path: "/user-profiles/preferences", // ← single “save all prefs” endpoint
+    //   handler: "user-profile.updatePreferences",
+    //   config: {
+    //     auth: {
+    //       mode: "required",
+    //       strategies: ["plugin::users-permissions.jwt"],
+    //       scope: [], // allow any scope
+    //     },
+    //   },
+    // },
+  ],
+};
