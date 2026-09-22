d2d9c79f366ee571b84741122756db9f52c3bdd6
JassyKMZ <203271569+JassyKMZ@users.noreply.github.com>
2026-09-22 14:53:53 +0200

    uploaded api fodler in the correct directory

---
+++ src/api/tipp-section/controllers/tipp-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tipp-section controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tipp-section.tipp-section');
